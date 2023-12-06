9657401815e8fa75424984a37acff25d5f82989b
Tiago Babo <tiago.babo@gmail.com>
2023-12-06 09:53:25 +0000

    Use zip and inject

--- advent-of-code-2023/6/part_1.rb
+++ advent-of-code-2023/6/part_1.rb
@@ -1,9 +1,8 @@
 lines = File.readlines('input.txt', chomp: true)
 times, max_distances = lines.map {|l| l.scan(/(\d+)/).flatten.map(&:to_i) }
-multiplication = 1
 
-times.each_with_index do |time, index|
-  multiplication *= (0..time).count { |t| t * (time - t) > max_distances[index] }
+result = times.zip(max_distances).inject(1) do |result, (time, max_distance)|
+  result * (0..time).count { |t| t * (time - t) > max_distance }
 end
 
-p multiplication
+p result
