ed3bd2e88d2c93ce305dc932f5d7993c7970fa21
Tiago Babo <tiago.babo@gmail.com>
2023-12-10 22:12:11 +0000

    Add alternative solution

--- advent-of-code-2023/10/part_2.rb
+++ advent-of-code-2023/10/part_2.rb
@@ -6,18 +6,19 @@ points << [s[0] - 1, s[1]] if %w(| F 7).include?(map[s[0] - 1][s[1]])
 points << [s[0] + 1, s[1]] if %w(| L J).include?(map[s[0] + 1][s[1]])
 points << [s[0], s[1] - 1] if %w(- F L).include?(map[s[0]][s[1] - 1])
 points << [s[0], s[1] + 1] if %w(- J 7).include?(map[s[0]][s[1] + 1])
-points = points.first(2)
 
-map[s[0]][s[1]] = 'J' # only works for my input
+map[s[0]][s[1]] = 'J' # replace S with direction
+
+points = points.first(2)
 
 until points.last == s
   previous, current = points.last(2)
 
   case map[current[0]][current[1]]
   when '|'
-    previous[0] > current[0] ?  points << [current[0] - 1, current[1]] : points << [current[0] + 1, current[1]]
+    previous[0] > current[0] ? points << [current[0] - 1, current[1]] : points << [current[0] + 1, current[1]]
   when '-'
-    previous[1] > current[1] ?  points << [current[0], current[1] - 1] : points << [current[0], current[1] + 1]
+    previous[1] > current[1] ? points << [current[0], current[1] - 1] : points << [current[0], current[1] + 1]
   when 'L'
     previous[0] == current[0] ? points << [current[0] - 1, current[1]] : points << [current[0], current[1] + 1]
   when 'J'
@@ -29,6 +30,20 @@ until points.last == s
   end
 end
 
+# Shoelace formula
+vertices = points.uniq.select { |x, y| %w(F 7 L J).include?(map[x][y]) }
+vertices << vertices.first
+shoelaces = vertices.each_cons(2).map do |(x1, y1), (x2, y2)|
+  x1 * y2 - x2 * y1
+end
+
+# Pick's theorem
+area = (shoelaces.reduce(:+) / 2).abs
+points_inside = (points.uniq.size / 2 - 1 - area).abs
+
+p points_inside
+
+# Alternative solution using Point in polygon
 count = points.uniq.sort.group_by { |p| p.first }.each.inject(0) do |acc, (row, values)|
   values = values.map(&:last)
   counter = 0
