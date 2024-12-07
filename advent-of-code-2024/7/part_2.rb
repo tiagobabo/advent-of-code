d8488fc295a719d11b76223e21d48f0e27d3887b
Tiago Babo <tiago.babo@gmail.com>
2024-12-07 10:14:13 +0000

    Add day 7 of 2024

---
+++ advent-of-code-2024/7/part_2.rb
@@ -0,0 +1,27 @@
+result = 0
+operators = [:+, :*, :special_concat]
+
+class Integer
+  def special_concat(other)
+    (self.to_s + other.to_s).to_i
+  end
+end
+
+File.readlines('input.txt', chomp: true).each do |line|
+  line_total, values = line.split(': ').then { |line_total, values| [line_total.to_i, values.split(' ').map(&:to_i)] }
+  operators_permutations = operators.repeated_permutation(values.size - 1)
+
+  operators_permutations.each do |operators|
+    total = values.drop(1).zip(operators).inject(values.first) do |acc, (value, operator)|
+      acc.send(operator, value)
+    end
+
+    if total == line_total
+      result += line_total
+
+      break
+    end
+  end
+end
+
+p result
